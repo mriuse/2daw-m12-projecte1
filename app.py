97b72fccc87f57dadfc43150e491618b629d0b87
mriuse <mriuse@fp.insjoaquimmir.cat>
2023-09-29 18:33:50 +0200

    DB initial implementation

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import sqlite3
-from flask import Flask, render_template, g
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
-DATABASE = "database.db" 
+
+DATABASE = "database.db"
+
+def connect_db():
+    db_path = DATABASE
+    connect = sqlite3.connect(db_path)
+    connect.row_factory = sqlite3.Row;
+    return connect
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def init():
+    return redirect(url_for("prod_list"))
+
+@app.route("/products/list")
+def prod_list():
+    with connect_db() as connect:
+        res = connect.execute("SELECT photo, title, seller_id, updated, price FROM products ORDER BY id ASC")
+        list = res.fetchall();
+    return render_template('prod_list.html', list = list)
